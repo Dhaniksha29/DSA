76cf6ae683b67b45d4d8dccf33364e86f8c80731
Dhaniksha <dhanikshabisht@gmail.com>
2026-07-22 23:39:18 +0530

    Time: 0 ms (100%), Space: 8 MB (51.42%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int total = a + b + c;
+        int mx = max({a, b, c});
+
+        return min(total / 2, total - mx);
+    }
+};
